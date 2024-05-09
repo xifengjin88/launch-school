ebb9e39b9118ed452e08959b68fe6ce12daadeec
xifeng jin <xifeng.jin88@gmail.com>
2024-05-09 17:30:59 -0500

    create data types exercises

---
+++ prep/books/intro-to-programming-with-python/chapts/data-types/exercise.py
@@ -0,0 +1,15 @@
+names = (
+  'Asta',
+  'Butterscotch',
+  'Pudding',
+  'Neptune',
+  'Darwin',
+)
+
+pets = {
+  "Asta": 'dog',
+  "Butterscotch": "cat",
+  "Pudding": "cat",
+  "Neptune": "fish",
+  "Darwin": "lizard",
+}
